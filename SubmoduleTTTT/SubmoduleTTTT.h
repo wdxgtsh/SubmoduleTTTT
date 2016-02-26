708d431965b963e6a790bb28313dbf0d47d79c95
zhaolei <zhaolei@allinmd.cn>
2016-02-26 20:23:21 +0800

    SubmoduleTTTT init

---
+++ SubmoduleTTTT/SubmoduleTTTT.h
@@ -0,0 +1,13 @@
+//
+//  SubmoduleTTTT.h
+//  SubmoduleTTTT
+//
+//  Created by zhaolei on 16/2/26.
+//  Copyright © 2016年 zhaolei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SubmoduleTTTT : NSObject
+
+@end
---
+++ SubmoduleTTTT/SubmoduleTTTT.m
@@ -0,0 +1,13 @@
+//
+//  SubmoduleTTTT.m
+//  SubmoduleTTTT
+//
+//  Created by zhaolei on 16/2/26.
+//  Copyright © 2016年 zhaolei. All rights reserved.
+//
+
+#import "SubmoduleTTTT.h"
+
+@implementation SubmoduleTTTT
+
+@end
